ec332b66f5a33d4e05669fef4f5f0e437ee03198
ProbstenHias <matthias.weilinger@gmx.de>
2018-12-20 22:46:48 +0100

    first test

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        var start = System.nanoTime();
+        int [] array = new int[25000000];
+        Arrays.fill(array, -1);
+        var end = System.nanoTime();
+        System.out.println("Time in sec: " + (end-start)/1e9);
     }
 }
